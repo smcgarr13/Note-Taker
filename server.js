8bfc747d0aeec5f83740cf17c30e64cd94a23d32
Sparkles <smcgarr13@yahoo.com>
2023-02-24 16:04:41 -0600

    create-routes

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+// Helper method for generating unique ids
+const uuid = require('./helpers/uuid');
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+// Middleware for parsing request
+app.use(express.urlencoded({ extended: true }));
+
+// app.use(express.static('public'));
+
+// Routes
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
+});
+
+// Route for getting saved notes
+app.get('./api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, 'db.jason'), 'utf8', (err, data) => {
+if (err) throw err;
+const notes = JSON.parse(data);
+res.json(notes);
+    });
+});
+
+//Route for adding new notes
+app.post('api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, 'db.json'), 'utf8', (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+        const newNote = req.body;
+        // Generate unique name/ID for new note
+        newNote.id = uuid();
+        // Add new note to array of notes
+        notes.push(newNote);
+        // write notes array to db.json file
+        fs.writeFile(path.join(__dirname, 'db.json'), JSON.stringify(notes), (err => {
+            if (err) throw err;
+            // return new note to client
+            res.json(newNote);
+        }));
+    });
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🫶`)
+);
