e89be341fd4a1838e1e44ed328faccec0dc6596e
smcgarr13 <117788958+smcgarr13@users.noreply.github.com>
2023-03-01 20:49:54 -0600

    Update html.js
    Cleaned up code

--- routes/html.js
+++ routes/html.js
@@ -1,58 +1,15 @@
 const express = require('express');
-const path = require('path');
-
 const router = express.Router();
-
+const path = require('path');
 
 // Serve the index.html file on the root URL
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
+// Serve the notes.html file on the /notes endpoint
 router.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public', 'notes.html'));
+  res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
 module.exports = router;
-
-
-
-
-
-
-// router.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
-// });
-
-// const express = require('express');
-// const path = require('path');
-// const fs = require('fs');
-// const router = express.Router();
-// const uuid = require('./helpers/uuid');
-
-// // Routes
-// router.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../public/index.html'))
-// });
-
-// router.get('/notes', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../public/notes.html'))
-// });
-
-// module.exports = router;
-
-// // module.exports = (app) => {
-// // const express = require('express');
-// // const path = require('path');
-// // const fs = require('fs');
-
-// // // HTML Routes
-// // app.get('/', (req, res) => {
-// //     res.sendFile(path.join(__dirname, './public/index.html'))
-// //   });
-  
-// //   app.get('*', (req, res) => {
-// //     res.sendFile(path.join(__dirname, './public/notes.html'))
-// //   });
-// // };
-// // //   module.exports = htmlRoute;
